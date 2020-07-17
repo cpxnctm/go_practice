8f5c2169aadaf4f1d6f90c39b8451b79e1893220
Jordan Chong <qc@Jordans-MacBook-Pro.local>
2020-07-17 15:50:19 -0600

    update

--- 001-practice/main.go
+++ 001-practice/main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"github.com/cpxnctm/go_practice/master/001-practice/tempconv"
+)
 
+func main(){
+	c := (tempconv.CToF(20))
+	f := (tempconv.FToC(20))
 
+	fmt.Printf("20 degrees Celsius is %v in Fahrenheit", c)
+	fmt.Printf("20 degrees Fahrenheit is %v in Celsius", f)
+}
