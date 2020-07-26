6b8a2cf69894507fd6476c0eed875842b5cc76aa
jordan <jordan@localhost.localdomain>
2020-07-26 07:00:49 -0600

    creating websocket handler

--- 01_server/main.go
+++ 01_server/main.go
@@ -1,52 +1,22 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
-	"net"
-	"os"
+	"net/http"
 )
 
 func main(){
-	li, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatalln(err)
 
-	}
-	defer li.Close()
 
-	for {
-		conn, err := li.Accept()
+}
+
+func Server(){
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ws, err := NewHandler(w, r)
 		if err != nil {
 			log.Println(err)
-			continue
 		}
-		go handle(conn)
+		if err = ws.Handshake(); err != nil {
+			log.Println(err)
+		}
 	}
-}
-
-func handle(conn net.Conn){
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan(){
-		ln := scanner.Text()
-		lg, err := os.Create("logging.txt")
-		if err != nil {
-			fmt.Println(err)
-		}
-		os.Open("logging.txt")
-		defer lg.Close()
-		w1, err := lg.WriteString(ln)
-		if err != nil {
-			fmt.Println(err)
-		fmt.Printf("%b", w1)
-			lg.Sync()
-		}
-
-		fmt.Println(ln)
-		fmt.Fprintf(conn, "You sent: %s\n", ln)
-
-
-	}
-	defer conn.Close()
-}
