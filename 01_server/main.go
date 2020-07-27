40f4573a5c55074633e1b10403549f66f5d7ad78
jordan <jordan@localhost.localdomain>
2020-07-27 12:50:52 -0600

    simple, working websocket-- need to capture input to a logfile

--- 01_server/main.go
+++ 01_server/main.go
@@ -1,22 +1,60 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 )
 
-func main(){
-
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
 
 }
 
-func Server(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ws, err := NewHandler(w, r)
+func main() {
+	fmt.Println("Websocket Open...")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+
+}
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Homepage!")
+}
+
+//check incoming origin
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	//upgrade the http connection to a websocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("Client Connected Successfully!")
+	reader(ws)
+}
+
+func reader(conn *websocket.Conn){
+	for {
+		messageType, p, err := conn.ReadMessage()
 		if err != nil {
 			log.Println(err)
+			return
 		}
-		if err = ws.Handshake(); err != nil {
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
 			log.Println(err)
+			return
 		}
 	}
+}
+
+
